38bf94726ebb819053104b3d0ff0cb23bf79f727
FranMack <franmack556@gmail.com>
2025-08-11 14:55:50 -0300

    FEAT: chat css adjudments

--- src/components/assistant-chat/components/MyMessage.tsx
+++ src/components/assistant-chat/components/MyMessage.tsx
@@ -9,8 +9,8 @@ export const MyMessage = ({ text }: Props) => {
         <div className="flex items-center justify-center h-16 w-16 text-[1.5rem] rounded-full bg-indigo-500 text-white flex-shrink-0">
           V
         </div>
-        <div className="relative mr-3 text-[1.2rem] bg-indigo-700 py-2 px-4 shadow rounded-xl text-white">
-          <div>{ text }</div>
+        <div className="relative max-w-[82%] mr-3 text-[1.2rem] bg-indigo-700 py-2 px-4 shadow rounded-xl text-white">
+          <div>{text}</div>
         </div>
       </div>
     </div>
