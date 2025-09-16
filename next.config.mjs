76221277eadad3c292adc6f37538c4204ff8924f
Carlos Zaburlin <zaburlin.carlos@gmail.com>
2025-09-16 13:26:41 -0300

    FEAT: update package.json & next  config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-    images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname:
-              "images.unsplash.com",
-          },
-       
-        ],
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
+    ],
+  },
 };
 
 export default nextConfig;
